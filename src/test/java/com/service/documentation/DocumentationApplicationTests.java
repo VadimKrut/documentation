8743ed17a25d2455b287b565f89bd28e8b1b891e
VadimKrut <vadim.zaletaev@mail.ru>
2024-10-16 19:47:04 +0300

    pobeda v1

---
+++ src/test/java/com/service/documentation/DocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.documentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
